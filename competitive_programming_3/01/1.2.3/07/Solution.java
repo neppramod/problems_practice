b5a153af64bbf3239ac1b49ee4d91f4c7d37bede
Pramod Nepal <nepalp@goldmail.etsu.edu>
2017-04-01 17:18:22 -0400

    Permutation of A..J

---
+++ competitive_programming_3/01/1.2.3/07/Solution.java
@@ -0,0 +1,28 @@
+/**
+Generate all possible permutations of {'A', 'B', 'C', ..., 'J'}, the first N = 10 letters in the alphabet (see Section 3.2.1).
+*/
+
+import java.util.*;
+import java.text.*;
+
+public class Solution
+{
+    public static void main(String[] args) {
+        // List<Character> charList = Arrays.asList('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J');
+        List<Character> charList = Arrays.asList('A', 'B', 'C');
+        permutation(charList, "");
+    }
+
+    public static void permutation(List<Character> charList, String prefix) {
+        if (charList.size() == 0) {
+            System.out.println(prefix);
+        } else {
+            for (int i = 0; i < charList.size(); i++) {
+                List<Character> newList = new ArrayList<>(charList);
+                newList.remove(i);
+                String newString = prefix + charList.get(i);
+                permutation(newList, newString);
+            }
+        }
+    }
+}
