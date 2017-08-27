132e2c9709cdf1896900ed457412a59a82c55f4f
neppramod <neppramod@gmail.com>
2017-08-27 09:25:09 -0400

    Activity selection problem (incomplete)

---
+++ tc/topcoder1/src/datascience/tutorial/greedy/JonnyFunPlan.java
@@ -0,0 +1,48 @@
+/*
+  Activity selection problems
+*/
+
+import java.util.*;
+
+public class JonnyFunPlan
+{
+    private static class Activity implements Comparable
+    {
+        Date start;
+        Date finish;
+
+        public Activity(Date s, Date f) { start = s; finish = f; }
+        public int compareTo(Object o) {
+            Activity a = (Activity) o;
+            return finish.compareTo(a.finish);
+        }
+    }
+
+    public ArrayList<Activity> selectedActivities(ArrayList<Activity> allActivities)
+    {
+        Collections.sort(allActivities);
+        ArrayList<Activity> selected = new ArrayList<Activity>();
+        if (allActivities.size() == 0) return selected;
+        selected.add(allActivities.get(0));
+
+        Activity j = allActivities.get(0);
+        for (int k = 1; k < allActivities.size(); k++) {
+            Activity i = allActivities.get(k);
+            if (i.start.compareTo(j.finish) >= 0) {
+                selected.add(i);
+                j = i;
+            }
+        }
+
+        return selected;
+    }
+
+    public static void main(String[] args)
+    {
+
+    }
+
+    private static ArrayList<Activity> prepareList() {
+
+    }
+}
