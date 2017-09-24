7786885294f9eca0dd6f1f6481bc7c172560a13c
neppramod <neppramod@gmail.com>
2017-09-24 14:17:02 -0400

    Brackets, mixtures etc

--- learning/dp/Mixtures.java
+++ learning/dp/Mixtures.java
@@ -33,12 +33,15 @@ public class Mixtures
     public static void main(String[] args)
     {
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int[] p = new int[N];
-        for (int i = 0; i < N; i++) {
-            p[i] = sc.nextInt();
-        }
+       
+       	while (sc.hasNext()) {
+            int N = sc.nextInt();
+            int[] p = new int[N];
+            for (int i = 0; i < N; i++) {
+                p[i] = sc.nextInt();
+            }
 
-        System.out.println(minSmoke(p));
+            System.out.println(minSmoke(p));
+        }
     }
 }
