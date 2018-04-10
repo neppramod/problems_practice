8c5f740ff40cb3e3218a6dd8682af070aeabcfd4
neppramod <neppramod@gmail.com>
2018-04-09 21:34:30 -0400

    Test emacs editor, no work related to PalindromeBoolean.java

--- pie/1301_Test_for_palindromic_permutations/PalindromeBoolean.java
+++ pie/1301_Test_for_palindromic_permutations/PalindromeBoolean.java
@@ -3,13 +3,13 @@ import java.util.*;
 /*
   WAP to test whether the letter forming a string can be permuted to form a palindromeboolean
 
-----
-Create boolean array of size 26
-1. if arr[c-'a'] == true, set to false else set to true
-2. Count all array and if size of true is more than 1 break and return false
+  ----
+  Create boolean array of size 26
+  1. if arr[c-'a'] == true, set to false else set to true
+  2. Count all array and if size of true is more than 1 break and return false
 
-Assuming all lower case
- */
+  Assuming all lower case
+*/
 
 public class PalindromeBoolean
 {
