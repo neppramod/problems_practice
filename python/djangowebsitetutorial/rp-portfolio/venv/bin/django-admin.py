83200ac7817c826553abe4bfecf9cc9057e82630
neppramod <neppramod@gmail.com>
2019-08-15 21:51:25 -0400

    Some files

--- python/djangowebsitetutorial/rp-portfolio/venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/pramod/Documents/Programming/projects/online_git_repos/neppramod/problems_practice/python/djangowebsitetutorial/rp-portfolio/venv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
