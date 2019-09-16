4ae74cfa860c927a3e5a3e79666ed046354f10fd
neppramod <neppramod@gmail.com>
2019-09-15 21:31:54 -0400

    ExternalSorting complete intial run

--- gfg/top_algorithms_for_interview_set1/heap/externalsorting/ExternalSorting.java
+++ gfg/top_algorithms_for_interview_set1/heap/externalsorting/ExternalSorting.java
@@ -37,7 +37,7 @@ public class ExternalSorting
         MinHeapNode[] minHeapNodeArr = new MinHeapNode[k];
         PriorityQueue<MinHeapNode> minHeap = new PriorityQueue<>();
 
-        int i;
+        int i;   // need i for max count, so can break from next while loop
         for (i = 0; i < k; i++) {
             if (!sc[i].hasNext()) {
                 break;
@@ -75,7 +75,121 @@ public class ExternalSorting
 
     }
 
-    public static void main(String args[]) throws Exception{
-        mergeKSortedFiles("outputfile.txt", 3);
+    // Using a merge-sort algorithm, create the initial run
+    // and divide them evenly among the output files
+    static void createIntialRuns(String inputFileName, int run_size, int num_ways) throws Exception
+    {
+        Scanner sc = new Scanner(new File(inputFileName));
+        BufferedWriter[] writers = new BufferedWriter[num_ways];
+
+        for (int i = 0; i < num_ways; i++) {
+            String outputFileName = String.format("%d", i);
+            writers[i] = new BufferedWriter(new FileWriter(outputFileName));
+        }
+
+        int[] arr = new int[run_size];
+        boolean more_input = true;
+        int next_output_file = 0;
+
+        int i;
+
+        while (more_input) {
+            // write run_size elements into arr from input
+            for (i = 0; i < run_size; i++) {
+                if (!sc.hasNext()) {
+                    more_input = false;
+                    break;
+                }
+
+                arr[i] = sc.nextInt();
+            }
+
+            mergeSort(arr, 0, i-1);
+
+            // write the records to the appropriate scratch output file
+            // can't assume that the loop runs to run_size
+            // since the last run's length may be less than run_size
+            for (int j = 0; j < i; j++) {
+                writers[next_output_file].write(arr[j] + " ");
+            }
+
+            next_output_file++;
+        }
+
+        // close input and output files
+        for (int j = 0; j < num_ways; j++) {
+            writers[j].close();
+        }
+        sc.close();
+    }
+
+    static void mergeSort(int[] arr, int l, int r)
+    {
+        if (l < r) {
+            int m = (l + r) / 2;
+            mergeSort(arr, l, m);
+            mergeSort(arr, m+1, r);
+
+            merge(arr, l, m, r);
+        }
+    }
+
+    static void merge(int[] arr, int l, int m, int r)
+    {
+        // Find sizes of two subarrays to be merged
+        int n1 = m - l + 1;
+        int n2 = r-m;
+
+        // create temp arrays
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+
+        // Copy data into temp arrays
+        for (int i= 0; i < n1; i++) {
+            L[i] = arr[l+i];
+        }
+
+        for (int j = 0; j < n2; j++) {
+            R[j] = arr[m + 1 + j];
+        }
+
+        // merge temp arrays, initialize indices of first and second subarrays
+        int i = 0, j = 0;
+        int k = l;   // initial index of merged subarray
+
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        // Copy remaining elements of L[] and R[] if any
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+
+    }
+
+    static void externalSort(String inputFile, String outputFile, int num_ways, int run_size) throws Exception
+    {
+        createIntialRuns(inputFile, run_size, num_ways);
+        mergeKSortedFiles(outputFile, num_ways);
+    }
+
+    public static void main(String args[]) throws Exception {
+        externalSort("input.txt", "output.txt", 3, 5);
     }
 }
