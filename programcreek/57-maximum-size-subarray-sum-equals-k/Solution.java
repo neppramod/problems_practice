b59ef2b4f8b8a6b8c3b441dcd6268f42918975f3
Pramod <neppramod@gmail.com>
2020-08-10 17:47:22 -0600

    Merge last

--- programcreek/57-maximum-size-subarray-sum-equals-k/Solution.java
+++ programcreek/57-maximum-size-subarray-sum-equals-k/Solution.java
@@ -4,31 +4,30 @@ public class Solution
 {
     public static void main(String[] args) {
         Solution sol = new Solution();
-		int[] nums = { -2, -1, 2, 1 };
-		int k = 1;
-
-		int result = sol.maxSubArrayLen(nums, k);
-		System.out.println(result);
+		int[] nums = { 1, -1, 5, -2, 3 };
+		int k = 3;
+		System.out.println(sol.maxSize(nums, k));
     }
     
-	public int maxSubArrayLen(int[] nums, int k) {
-		HashMap<Integer, Integer> map = new HashMap<>();
+	int maxSize(int[] nums, int k) {
+		if (nums == null || nums.length == 0) {
+			return 0;
+		}
 
-		maxLen = 0;
-		int sum = 0;
 		int n = nums.length;
-
+		int maxSize = 0;
+		int sum = 0;
+		HashMap<Integer, Integer> map = new HashMap<>();
+		
 		for (int i = 0; i < n; i++) {
 			sum += nums[i];
 
 			if (sum == k) {
-				maxLen = Math.max(maxLen, i + 1);
+				maxSize = Math.max(maxSize, i + 1);
 			}
 
-			int diff = sum - k;
-
-			if (map.containsKey(diff)) {
-				maxLen = Math.max(maxLen, i - map.get(diff));
+			if (map.containsKey(sum - k)) {
+				maxSize = Math.max(maxSize, i - map.get(sum - k));
 			}
 
 			if (!map.containsKey(sum)) {
@@ -36,7 +35,6 @@ public class Solution
 			}
 		}
 
-		return maxLen;
-		
+		return maxSize;
 	}
 }
