cc779f4b9f86fbeda8bd49f692b798035cbade19
Pramod <neppramod@gmail.com>
2020-08-09 21:19:48 -0600

    Maximum size subarray sum equals k

--- programcreek/57-maximum-size-subarray-sum-equals-k/Solution.java
+++ programcreek/57-maximum-size-subarray-sum-equals-k/Solution.java
@@ -4,38 +4,39 @@ public class Solution
 {
     public static void main(String[] args) {
         Solution sol = new Solution();
-		int[] nums = {1, -1, 5, -2, 3};
-		int k = 3;
-		int answer = sol.maxSubArrayLen(nums, 3);
-		System.out.println(answer);
+		int[] nums = { -2, -1, 2, 1 };
+		int k = 1;
+
+		int result = sol.maxSubArrayLen(nums, k);
+		System.out.println(result);
     }
     
-    
 	public int maxSubArrayLen(int[] nums, int k) {
-        		int maxSize = 0;
-
-		int sum = 0;
 		HashMap<Integer, Integer> map = new HashMap<>();
 
+		maxLen = 0;
+		int sum = 0;
 		int n = nums.length;
+
 		for (int i = 0; i < n; i++) {
 			sum += nums[i];
 
 			if (sum == k) {
-				maxSize = Math.max(maxSize, i + 1);
-			} 
-            
-            if (map.containsKey(sum-k)) {
-				maxSize = Math.max(maxSize, i - map.get(sum-k));
+				maxLen = Math.max(maxLen, i + 1);
+			}
+
+			int diff = sum - k;
+
+			if (map.containsKey(diff)) {
+				maxLen = Math.max(maxLen, i - map.get(diff));
 			}
 
 			if (!map.containsKey(sum)) {
 				map.put(sum, i);
 			}
 		}
+
+		return maxLen;
 		
-
-		return maxSize;
-    }
-
+	}
 }
