61b707ac04b60a890d2800fc46cb66867c9e7abb
Pramod <neppramod@gmail.com>
2020-08-12 19:08:35 -0600

    Find all duplicates

--- leetcode/monthly/august/august-06/Solution.java
+++ leetcode/monthly/august/august-06/Solution.java
@@ -4,21 +4,21 @@ import java.util.*;
 // Element is within range, so negate, and if we see again add to answer
 public class Solution
 {
-    public static void main(String[] args) {
-        Solution sol = new Solution();
-		int[] nums = {4, 3, 2, 7, 8, 2, 3, 1};
+    public static void main(final String[] args) {
+        final Solution sol = new Solution();
+		final int[] nums = {4, 3, 2, 7, 8, 2, 3, 1};
 		System.out.println(sol.findAllDuplicates(nums));
     }
 
-	List<Integer> findAllDuplicates(int[] nums) {
-		List<Integer> result = new ArrayList<>();
+	List<Integer> findAllDuplicates(final int[] nums) {
+		final List<Integer> result = new ArrayList<>();
 
 		if (nums == null || nums.length == 0) {
 			return result;
 		}
 		
 		
-		int n = nums.length;
+		final int n = nums.length;
 		for (int i = 0; i < n; i++) {
 			if (nums[Math.abs(nums[i]) - 1] < 0) {
 				result.add(Math.abs(nums[i]));
