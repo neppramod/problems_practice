c58cc667d079193b079ae9461b303cf339866005
Pramod <neppramod@gmail.com>
2020-09-05 19:06:15 -0600

    Permutation in string

--- programcreek/permutation-in-string/Solution.java
+++ programcreek/permutation-in-string/Solution.java
@@ -10,31 +10,31 @@ empty map used for second
 for each character in s2
    -- if cur char is not equal to one in s1, i = j + 1, empty tmp map, contineu
    -- else
-        get current count of character, if it is 0, or less than cur char in s2
-        - increment in tmp map
-        - if j - i + 1 == s1.length() return true
-        if current count is greater tha length
-           while (i < j):
-                 if (cur s2 charactes at i and j are equal incrment i++, break)
-                 else decrement character at i from tmp map, increment i
+		get current count of character, if it is 0, or less than cur char in s2
+		- increment in tmp map
+		- if j - i + 1 == s1.length() return true
+		if current count is greater tha length
+		   while (i < j):
+				 if (cur s2 charactes at i and j are equal incrment i++, break)
+				 else decrement character at i from tmp map, increment i
 
 */
 
 
 
 import java.util.*;
- 
+
 public class Solution
 {
-    public static void main(String[] args) {
-        Solution sol = new Solution();
+	public static void main(String[] args) {
+		Solution sol = new Solution();
 		String s1 = "ab";
 		String s2 = "eidbaooo";
 		boolean result = sol.checkInclusion(s1, s2);
 		System.out.println(result);
-    }
-    
-    public boolean checkInclusion(String s1, String s2) {
+	}
+
+	public boolean checkInclusion(String s1, String s2) {
 		HashMap<Character, Integer> map = new HashMap<>();
 
 		for (int i = 0; i < s1.length(); i++) {
@@ -56,7 +56,7 @@ public class Solution
 				tmpMap.clear();
 				continue;
 			} else {
-				
+
 				int count = tmpMap.getOrDefault(c, 0);
 				if (count == 0 || count < map.get(c)) {
 					tmpMap.put(c, count + 1);
@@ -77,7 +77,7 @@ public class Solution
 					}
 				}
 			}
-			
+
 		}
 
 		return false;
