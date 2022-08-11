2d3021e72eb0f8ac4dbea37c4e5ff855dd748e29
Pramod <neppramod@gmail.com>
2022-08-10 20:08:33 -0600

    Delete kth node in linked list

---
+++ gfg/recursion_problems_practice_gfg/linkedlist/DeleteKthNode.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+public class DeleteKthNode {
+    public static void main(String[] args) {
+	    DeleteKthNode sol = new DeleteKthNode();
+		Node r = c(9);
+        r.next = c(8);
+        r.next.next = c(3);
+        r.next.next.next = c(5);
+        r.next.next.next.next = c(2);
+        r.next.next.next.next.next = c(1);
+        int k = 4;
+        sol.deleteNode(r, k);
+        sol.printList(r);
+	}
+	
+    static Node c(int v) {
+        return new Node(v);
+    }
+
+    void deleteNode(Node r, int k) {
+        if (r == null || r.next == null) {
+            return;
+        } else if (k == 2) {
+            r.next = r.next.next;
+        } else {
+            deleteNode(r.next, k - 1);
+        }
+    }
+
+    void printList(Node r) {
+        if (r != null) {
+            System.out.printf(r.val + " -> ");
+            printList(r.next);
+        }
+    } 
+}
+
+
+
+
+class Node {
+    int val;
+    Node next;
+
+    public Node(int v) {
+        val = v;
+    }
+}
